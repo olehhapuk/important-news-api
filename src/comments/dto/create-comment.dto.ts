3540320f0ef114c6e9c19cd672e368a66a05a5b7
olehhapuk <oleggapuk20@gmail.com>
2026-02-02 18:28:07 +0200

    added create comment dto

--- src/comments/dto/create-comment.dto.ts
+++ src/comments/dto/create-comment.dto.ts
@@ -1,5 +1,22 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateCommentDto {
+  @ApiProperty({
+    type: 'string',
+    example: 'John Doe',
+  })
   author: string;
+
+  @ApiProperty({
+    type: 'string',
+    example: 'This is a comment text.',
+  })
   text: string;
+
+  @ApiProperty({
+    type: 'string',
+    format: 'uuid',
+    example: 'test-id-1',
+  })
   newsId: string;
 }
